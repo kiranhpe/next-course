c85f909ede332478452374eff1ec6ae89578e35d
kirantatkal1 <kirantatkal@gmail.com>
2021-10-30 11:17:28 +0530

    responsive

--- components/course.js
+++ components/course.js
@@ -1,12 +1,11 @@
 import { Card } from "../ui-kit/card";
 import { LinkButton } from "../ui-kit/link-button";
-import { FaArrowRight } from "react-icons/fa";
+import { FaArrowRight, FaStar } from "react-icons/fa";
 
 import styles from "./course.module.scss";
 import Rating from "react-rating";
 
 export const Course = ({ course, authors, isAdvancedPage }) => {
-
   return (
     <Card>
       <title>{course.name}</title>
@@ -18,17 +17,26 @@ export const Course = ({ course, authors, isAdvancedPage }) => {
           </div>
         )}
         <span className={[...[styles.rating]].join(" ")}>
-          <Rating initialRating={course.rating} readonly />
+          <span className={styles.desktopRating}>
+            <Rating initialRating={course.rating} readonly />
+          </span>
+          <span className={styles.mobileRating}>
+            {course.rating} <FaStar />
+          </span>
         </span>
       </div>
+
       <div className={styles.courseDetails}>
         <div>
           <p>{course.description}</p>
-          {course.category.split(",").map((catogory,index) => (
-            <span className={styles.tags} key={catogory+'-'+index}>{catogory}</span>
+          {course.category.split(",").map((catogory, index) => (
+            <span className={styles.tags} key={catogory + "-" + index}>
+              {catogory}
+            </span>
           ))}
           <p className={styles.author}>
-            {authors && "- " + authors.find(x =>x.id === course.id)?.name}
+            {authors &&
+              "- " + authors.find((x) => x.id === course.author)?.name}
           </p>
         </div>
         <div className={styles.linkButtonContainer}>
